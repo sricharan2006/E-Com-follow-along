a0501867219625c890d2f63096833ba05629763e
sri charan bukka <charan.bukka@gmail.com>
2025-02-21 16:06:15 +0530

    done with M8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { LoginPage, SignupPage } from './Routes.jsx'; // ✅ Import both LoginPage and SignupPage
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage, SignupPage, Home } from "./Routes.jsx"; 
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Home />} /> {/* ✅ Home Route */}
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/signup" element={<SignupPage />} /> 
             </Routes>
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,3 +1,5 @@
-import LoginPage from './Pages/Login'
-import SignupPage from './Pages/Signup'
-export {LoginPage,SignupPage};
+import LoginPage from "./Pages/Login";
+import SignupPage from "./Pages/Signup";
+import Home from "./Pages/home"; 
+
+export { LoginPage, SignupPage, Home };
